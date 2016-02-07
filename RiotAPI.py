0a5350d5b92028d02eb4cb417614a9f30069c5f4
philipz1 <philipz@uchicago.edu>
2016-02-06 19:03:12 -0600

    Update RiotAPI.py

--- RiotAPI.py
+++ RiotAPI.py
@@ -5,6 +5,9 @@ import sqlite3
 
 key = '9df451c2-91bc-4584-99f5-87334af39c2a'
 key2 = '8015aa1d-df1d-4cda-b319-dffcbcf2f708'
+key3 = 'fa134dbe-f2ab-4ec8-87f6-3a653298a272'
+
+SQL_columns = ['summoner_id', 'summoner_name', 'match_id', 'season', 'time_stamp', 'champion', 'winner']
 
 summoner_name = 'hanazono'
 summoner_name = summoner_name.replace(' ','')
@@ -14,6 +17,9 @@ summoner_info_not_byte = summoner_info.readall().decode('utf-8')
 summoner_id = json.loads(summoner_info_not_byte)[summoner_name]['id']
 
 def get_champion_id_table(key):
+	'''
+	Returns a dictionary with champions_id as keys and the champion name as values
+	'''
 	champ_api = urllib.request.urlopen('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key={}'.format(key))
 	champ_api_not_byte = champ_api.readall().decode('utf-8')
 	champ_info = json.loads(champ_api_not_byte)['data']
@@ -26,20 +32,28 @@ def get_champion_id_table(key):
 champion_id_table = get_champion_id_table(key)
 
 def get_matches(summoner_id, key):
+	'''
+	Returns a list of matches for a given summoner. The stats for the summoner are returned, and nothing else
+	'''
 	match_list = []
 	matches_info = urllib.request.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/{}?rankedQueues=RANKED_SOLO_5x5&api_key={}'.format(summoner_id,key))
 	matches_info_not_byte = matches_info.readall().decode('utf-8')
 	matches = json.loads(matches_info_not_byte)
 	# print(matches['matches'][0].keys())
-	for match in matches['matches'][0:20]:
+	for match in matches['matches'][0:5]:
 		# time.sleep(1.2)
 		match_list.append(get_match_info_for_summoner(match, key, summoner_name))
 
 	return match_list
 
 def get_match_info_for_summoner(match, key, summoner_name):
+	'''
+	Parses out the summoner-specific information. Returns a dictionary
+	'''
 	match_id = match['matchId']
+
 	match_info = urllib.request.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/match/{}?api_key={}'.format(match_id, key))
+	time.sleep(1.2)
 	match_info_not_byte = match_info.readall().decode('utf-8')
 	match_json = json.loads(match_info_not_byte)
 	#print(match['participants'][0])
@@ -52,25 +66,24 @@ def get_match_info_for_summoner(match, key, summoner_name):
 	match_info_for_summoner['match_id'] = match['matchId']
 	match_info_for_summoner['season'] = match['season']
 	match_info_for_summoner['timestamp'] = match['timestamp']
-	#print(match_info_for_summoner)
-	time.sleep(1.2)
+
 	return match_info_for_summoner
 
 def add_to_SQL(s_id, s_name, match_list):
-	# cur.execute("insert into contacts (name, phone, email) values (?, ?, ?)",
- #            (name, phone, email))
-	conn = sqlite3.connect('info.db')
+	values = []
 	for match in match_list:
-		try:
-			conn.execute('INSERT INTO defaultinfo VALUES (?, ?, ?, ?, ?, ?, ?)',\
-				(s_id, s_name, match['match_id'], match['season'], match['timestamp'], champion_id_table[match['championId']], match['stats']['winner']))	
-		except Exception:
-			conn.execute('''CREATE TABLE defaultinfo (summoner_id, summoner_name, match_id, season, time_stamp, champion, winner)''')
-			conn.execute('INSERT INTO defaultinfo VALUES (?, ?, ?, ?, ?, ?, ?)',\
-				(s_id, s_name, match['match_id'], match['season'], match['timestamp'], champion_id_table[match['championId']], match['stats']['winner']))		
+		values.append((s_id, s_name, match['match_id'], match['season'], match['timestamp'], champion_id_table[match['championId']], match['stats']['winner']))
+
+	conn = sqlite3.connect('info4.db')
+	try:
+		conn.executemany('INSERT INTO defaultinfo VALUES ({})'.format(','.join('?' * len(values[0]))), (values))
+	except Exception:
+		conn.execute('''CREATE TABLE defaultinfo ({})'''.format(','.join(SQL_columns)))
+		conn.executemany('INSERT INTO defaultinfo VALUES ({})'.format(','.join('?' * len(values[0]))), (values))	
 
 	conn.commit()
 	conn.close()
+
 
 pie = get_matches(summoner_id, key)
 add_to_SQL(summoner_id, summoner_name, pie)
