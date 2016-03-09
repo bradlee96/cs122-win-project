92dfb2e116bc36d115f464e4f6719989f1a6b3b6
Brad <bradleeusa@gmail.com>
2016-03-08 20:40:38 -0600

    Removed unnecessary files

--- leaguetracker/leaguetracker/settings.py
+++ leaguetracker/leaguetracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e@v+$r(j=p6qcx&j@@2jekvvf^$f6pnd=sbcbi4_=p8(@5ddxe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
